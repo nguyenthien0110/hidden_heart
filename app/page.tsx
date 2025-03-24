48ac6b57f54b92e46af18401739388ce68160f8a
ThienNT <Nguyenthien11082005@gmail.com>
2025-03-24 22:19:39 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="flex w-full h-screen items-center justify-center text-8xl">
+        Hi Baby
+      </h1>
+    </>
+  );
+}
